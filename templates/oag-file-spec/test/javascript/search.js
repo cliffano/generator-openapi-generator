9dc3ed4eeb2ddfca89806894447bc4c3cfb6b9e9
Cliffano Subagio <cliffano@gmail.com>
2024-06-16 21:39:55 +1000

    Add javascript and python tests.

--- templates/oag-file-spec/test/javascript/search.js
+++ templates/oag-file-spec/test/javascript/search.js
@@ -1,73 +1,18 @@
 const assert = require('assert');
-const Ipify = require('{{rubyPkgName project_name}}');
-const api = new Ipify.DefaultApi();
+const OpenapiJsClient = require('openapi-js-client');
+const api = new OpenapiJsClient.DefaultApi()
 
-describe('ipify', function() {
-  describe('without format', function() {
-    it('should return IP address in plain text', function(done) {
-      var callback = function(error, data, response) {
-        assert.equal(error, null);
-        // TODO: disabled for now due to undefined data being returned
-        // assert.equal(typeof data, 'string');
-        // see https://github.com/{{github_id}}/{{project_id}}/issues/13
-        assert.equal(typeof response.text, 'string');
-        assert.equal(response.res.statusCode, 200);
+describe('google', function() {
+  it('should search with the query', function(done) {
+    var q = 'OpenAPI Generator web site';
+    var callback = function(error, data, response) {
+      if (error) {
+        done(error);
+      } else {
+        console.log('API called successfully. Returned data: ' + data);
         done();
-      };
-      var opts = {
-      };
-      api.getIp(opts, callback);
-    });
-  });
-  describe('with JSON format', function() {
-    it('should return IP address within an object', function(done) {
-      var callback = function(error, data, response) {
-        assert.equal(error, null);
-        try {
-          assert.equal(typeof data.ip, 'string');
-          assert.equal(response.res.statusCode, 200);
-          done();
-        } catch (e) {
-          assert.fail(e);
-        }
-      };
-      var opts = {
-        'format': 'json'
-      };
-      api.getIp(opts, callback);
-    });
-  });
-  describe('with JSONP format', function() {
-    it('should return IP address as function string', function(done) {
-      var callback = function(error, data, response) {
-        assert.equal(error, null);
-        // TODO: disabled for now due to undefined data being returned
-        // see https://github.com/{{github_id}}/{{project_id}}/issues/13
-        // assert.equal(typeof data, 'string');
-        assert.equal(response.res.statusCode, 200);
-        done();
-      };
-      var opts = {
-        'format': 'jsonp'
-      };
-      api.getIp(opts, callback);
-    });
-  });
-  describe('with JSONP format and callback function name', function() {
-    it('should return IP address as function string', function(done) {
-      var callback = function(error, data, response) {
-        assert.equal(error, null);
-        // TODO: disabled for now due to undefined data being returned
-        // see https://github.com/{{github_id}}/{{project_id}}/issues/13
-        // assert.equal(typeof data, 'string');
-        assert.equal(response.res.statusCode, 200);
-        done();
-      };
-      var opts = {
-        'format': 'jsonp',
-        'callback': 'foobar'
-      };
-      api.getIp(opts, callback);
-    });
+      }
+    };
+    api.search(q, callback);
   });
 });
